730a4dcb4b39749b10c677dd94b5b2594a1c7a81
Pimo.Lin <iltetnow@gmail.com>
2018-06-03 21:33:29 +0800

    Refactor(script): Refine the Run script
    Refactor(script): Refine the Run script

--- RunPTTCrawler.sh
+++ RunPTTCrawler.sh
@@ -1,1 +1,11 @@
-python3.5 ptt_crawlet.py $1 $2 $3
+#python3.5 ptt_crawlet.py $1 $2 $3
+#python3.5 ptt_crawlet.py hsinchu 贈送 3
+#python3.5 ptt_crawlet.py give 新竹 3
+#python3.5 ptt_crawlet.py give 屏東 3
+#python3.5 ptt_crawlet.py pingtung 贈送 3
+
+python ptt_crawlet.py hsinchu 贈送 3
+python ptt_crawlet.py give 新竹 3
+python ptt_crawlet.py give 屏東 3
+python ptt_crawlet.py pingtung 贈送 3
+
